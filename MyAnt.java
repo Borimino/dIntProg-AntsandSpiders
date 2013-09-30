e8e325bb061269c5a3af768828344b94a547b74a
Borimino <markusandreassen1994@gmail.com>
2013-09-30 18:19:54 +0200

    1362.8 Signed-off-by: Borimino <markusandreassen1994@gmail.com>

--- MyAnt.java
+++ MyAnt.java
@@ -8,17 +8,20 @@ import java.util.*;
 
 public class MyAnt extends Ant
 {
- 
+
     private double wallFactor = 2000;
     private double antFactor = 60;
     private double sugarFactor = 20;
     private double spiderFactor = 200;
     private int wallDistance = 25;
+    private int wallDistanceMin = 15;
+    private int wallDistanceMax = 38;
     private int antDistance = 8;
     private int sugarDistance = 700;
     private int spiderDistance = 25;
+    private int spiderDistance2 = 100;
 
-     /**
+    /**
      * Find out which forces are effecting the ant.
      * (1) Avoid running into the wall.
      * Add as your progress...
@@ -28,15 +31,15 @@ public class MyAnt extends Ant
      */
     public Vector getForces()
     {
-        Vector f = new Vector();     
+        Vector f = new Vector(); 
+        setWallDistance();    
         f = f.add(avoidWall());
         f = f.add(avoidAnt());
         f = f.add(findSugar());
         f = f.add(avoidSpider());
         return f;
     }
-    
-    
+
     /**
      * Avoid moving into any of the walls.
      * Walls that a close should be avoided more rapidly than walls far away.
@@ -44,13 +47,13 @@ public class MyAnt extends Ant
      * @return a vector that points away from the nearest walls.
      * 
      */
-    
+
     private Vector avoidWall()
     {
         Vector wallV = new Vector();
         if(distanceToTopWall() <= wallDistance)
             wallV = wallV.add(new Vector(0, 1/distanceToTopWall())); // avoid top wall 
-        
+
         if(distanceToBottomWall() <= wallDistance)
             wallV = wallV.add(new Vector(0, -1/distanceToBottomWall()));// avoid bottom wall, not yet implemented
 
@@ -59,14 +62,14 @@ public class MyAnt extends Ant
 
         if(distanceToRightWall() <= wallDistance)
             wallV = wallV.add(new Vector(-1/distanceToRightWall(),0)); // avoid right wall
-        
+
         return wallV.scale(wallFactor);
     }   
-    
+
     private Vector avoidAnt()
     {
         Vector antV = new Vector();
-        
+
         List<Ant> ants = getAnts(antDistance);
         for(Ant a : ants)
         {
@@ -74,11 +77,11 @@ public class MyAnt extends Ant
         }
         return antV.scale(antFactor);
     }
-    
+
     private Vector findSugar()
     {
         Vector sugarV = new Vector();
-        
+
         List<Sugar> sugars = getSugar(sugarDistance);
         for(Sugar s : sugars)
         {
@@ -86,11 +89,11 @@ public class MyAnt extends Ant
         }
         return sugarV.scale(sugarFactor);
     }
-    
+
     private Vector avoidSpider()
     {
         Vector spiderV = new Vector();
-        
+
         List<Spider> spiders = getSpider(spiderDistance);
         for(Spider s : spiders)
         {
@@ -98,10 +101,25 @@ public class MyAnt extends Ant
         }
         return spiderV.scale(spiderFactor);
     }
-    
+
+    private void setWallDistance()
+    {
+        List<Spider> spiders = getSpider(spiderDistance);
+        for(Spider s : spiders)
+        {
+            if(getDistanceToSpider(s) < spiderDistance2)
+            {
+                wallDistance = wallDistanceMax;
+            } else
+            {
+                wallDistance = wallDistanceMin;
+            }
+        }
+    }
+
     public String getCreator()
     {
-        return "201304194";
+        return "Markus Andreassen 201304194";
     }
 
 }   
