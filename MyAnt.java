5764b34971b91e9d75df23599a1e35f5f8cfbd13
Borimino <markusandreassen1994@gmail.com>
2013-10-01 08:43:59 +0200

    1297.2 Signed-off-by: Borimino <markusandreassen1994@gmail.com>

--- MyAnt.java
+++ MyAnt.java
@@ -9,11 +9,17 @@ import java.util.*;
 public class MyAnt extends Ant
 {
 
+    /**
+     * The factors to multiply the different forces with.
+     */
     private double wallFactor = 2000;
     private double antFactor = 60;
     private double sugarFactor = 20;
     private double spiderFactor = 200;
-    private int wallDistance = 25;
+    /**
+     * The distances the forces work within.
+     */
+    private int wallDistance = 15;
     private int wallDistanceMin = 15;
     private int wallDistanceMax = 38;
     private int antDistance = 8;
@@ -23,8 +29,6 @@ public class MyAnt extends Ant
 
     /**
      * Find out which forces are effecting the ant.
-     * (1) Avoid running into the wall.
-     * Add as your progress...
      * 
      * @return The total of the forces
      * 
@@ -42,9 +46,9 @@ public class MyAnt extends Ant
 
     /**
      * Avoid moving into any of the walls.
-     * Walls that a close should be avoided more rapidly than walls far away.
+     * Only avoids the walls that are closer than <i>wallDistance</i>.
      * 
-     * @return a vector that points away from the nearest walls.
+     * @return A vector that points away from the nearest walls.
      * 
      */
 
@@ -66,6 +70,13 @@ public class MyAnt extends Ant
         return wallV.scale(wallFactor);
     }   
 
+    /**
+     * Avoid moving into any other ants.
+     * Only avoids ants that are closer than <i>antDistance</i>.
+     * 
+     * @return A vector that points away from all ants within range
+     * 
+     */
     private Vector avoidAnt()
     {
         Vector antV = new Vector();
@@ -78,6 +89,13 @@ public class MyAnt extends Ant
         return antV.scale(antFactor);
     }
 
+    /**
+     * Move towards the nearest sugar.
+     * Only considers sugars within <i>sugarDistance</i>.
+     * 
+     * @return A vector that points towards sugar
+     * 
+     */
     private Vector findSugar()
     {
         Vector sugarV = new Vector();
@@ -90,6 +108,13 @@ public class MyAnt extends Ant
         return sugarV.scale(sugarFactor);
     }
 
+    /**
+     * Avoid the spider.
+     * Only considers the spider if it is within <i>spiderDistance</i>.
+     * 
+     * @return A vector that points away from the spider
+     * 
+     */
     private Vector avoidSpider()
     {
         Vector spiderV = new Vector();
@@ -102,6 +127,9 @@ public class MyAnt extends Ant
         return spiderV.scale(spiderFactor);
     }
 
+    /**
+     * Sets the <i>wallDistance</i> depending on the closenes of the spider.
+     */
     private void setWallDistance()
     {
         List<Spider> spiders = getSpider(spiderDistance);
